9873b6c228855f2d05209eb3e90c8bc2d0e31e28
method <mamatisaevanurisa@icloud.com>
2023-07-20 15:46:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import com.sun.jdi.Value;
+
+public class Main {
+    public static void main(String[] args) {
+
+       Days [] days = Days.values();
+        for (Days day : days) {
+            System.out.println(day);
+
+        }
+    }
+}
